35d49f275f31c53eeb0fcd71168470893de309cc
emreisler <emre.isler.9@gmail.com>
2024-03-15 23:30:10 +0300

    deleted

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,15 @@
 function Main() {
-    return(
+    return (
         <div className="app-main">
             <div className="app-main-note-edit">
                 <input type="text" id="title" autoFocus />
-                <textarea id="body" placeholder="Write your note here"/>
+                <textarea id="body" placeholder="Write your note here" />
             </div>
-            <div className="portfolio">
-                <h2>Portfolio</h2>
-                <p>Checkout the stuff I've done!</p>
-            </div>
-
-
             <div className="app-main-note-preview">
                 <h1 className="preview-title">TITLE</h1>
-                    <div className="markdown-preview">
-                        note preview
-                    </div>
+                <div className="markdown-preview">
+                    note preview
+                </div>
 
             </div>
 
